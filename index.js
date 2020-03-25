51d83e1a4a66a53f487a419b5e14d2769b97857b
JaysonD <jaduff20@gmail.com>
2020-03-25 18:13:11 -0400

    added axios call for username, returns repos, need to change

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-function writeToFile(fileName, data) {
-}
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+      const repoNamesStr = repoNames.join("\n");
 
-function init() {
+      fs.writeFile("READMEtest.md", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
 
-}
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+const questions = [];
+
+function writeToFile(fileName, data) {}
+
+function init() {}
 
 init();
