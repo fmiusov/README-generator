199d70570f026c2dc7e30fa96958dcfdea848967
JaysonD <jaduff20@gmail.com>
2020-03-26 15:13:36 -0400

    wrapped everything within function init, called init at end

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your project title?",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "What is your project description?",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "What type of license is this?",
-      name: "license"
-    }
-  ])
-  .then(function(answer) {
-    fs.writeFile(
-      "READMETEST.md",
-      `# Project Title: ${answer.title}\n\n## Description: ${answer.description}\n\n## Table of Contents  
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "What is your project description?",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "What type of license is this?",
+        name: "license"
+      }
+    ])
+    .then(function(answer) {
+      fs.writeFile(
+        "READMETEST.md",
+        `# Project Title: ${answer.title}\n\n## Description: ${answer.description}\n\n## Table of Contents  
     1. Installation
     
     2. Usage
@@ -35,53 +36,48 @@ inquirer
     5. Tests
     
     6. GitHub Info:\n\n `,
-      function(err) {
-        if (err) {
-          return console.log(err);
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
         }
-      }
-    );
-  })
-  .then(function() {
-    inquirer
-      .prompt({
-        type: "input",
-        message: "Enter your GitHub username:",
-        name: "username"
-      })
-      .then(function({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
-        const queryEmailUrl = `https://api.github.com/users/${username}/events/public`;
-        axios.get(queryUrl).then(function(res) {
-          fs.appendFile(
-            "READMETEST.md",
-            `\n![profile avatar](${res.data.avatar_url})\n`,
-            function(err) {
-              if (err) {
-                throw err;
+      );
+    })
+    .then(function() {
+      inquirer
+        .prompt({
+          type: "input",
+          message: "Enter your GitHub username:",
+          name: "username"
+        })
+        .then(function({ username }) {
+          const queryUrl = `https://api.github.com/users/${username}`;
+          const queryEmailUrl = `https://api.github.com/users/${username}/events/public`;
+          axios.get(queryUrl).then(function(res) {
+            fs.appendFile(
+              "READMETEST.md",
+              `\n![profile avatar](${res.data.avatar_url})\n`,
+              function(err) {
+                if (err) {
+                  throw err;
+                }
               }
-            }
-          );
-        });
-        axios.get(queryEmailUrl).then(function(pushinfo) {
-          fs.appendFile(
-            "READMEtest.md",
-            //sniffing out the email address from their most recent public push payload
-            `Email me at: ${pushinfo.data[0].payload.commits[0].author.email}\n`,
-            function(err) {
-              if (err) {
-                throw err;
+            );
+          });
+          axios.get(queryEmailUrl).then(function(pushinfo) {
+            fs.appendFile(
+              "READMEtest.md",
+              //sniffing out the email address from their most recent public push payload
+              `Email me at: ${pushinfo.data[0].payload.commits[0].author.email}\n\n`,
+              function(err) {
+                if (err) {
+                  throw err;
+                }
               }
-            }
-          );
+            );
+          });
         });
-      });
-  });
+    });
+}
 
-// const questions = [];
-
-// function writeToFile(fileName, data) {}
-
-// function init() {}
-
-// init();
+init();
