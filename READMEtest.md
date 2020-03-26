744a98ce30c5d69cd472100d922f355a9be252f5
JaysonD <jaduff20@gmail.com>
2020-03-26 15:00:59 -0400

    readme now generating main body content correctly and in correct format, just need to link the github functionality and badge

--- READMEtest.md
+++ READMEtest.md
@@ -1,3 +1,16 @@
-#Project Title: Projecto Whatever
-![profile avatar](https://avatars0.githubusercontent.com/u/59235205?v=4)
-Email me at: jaduff20@gmail.com
+# Project Title: this time with feeling
+
+## Description: gahhh
+
+## Table of Contents  
+    1. Installation
+    
+    2. Usage
+    
+    3. License - mit
+    
+    4. Contributing
+    
+    5. Tests
+    
+    6. GitHub Info: 
--- index.js
+++ index.js
@@ -2,53 +2,91 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt({
+inquirer.prompt([
+  {
     type: "input",
     message: "What is your project title?",
     name: "title"
-  })
-  .then(function({ title }) {
-    fs.appendFile("READMEtest.md", `#Project Title: ${title}\n`, function(err) {
-      if (err) {
-        throw err;
-      }
-    });
-  })
-  .then(function() {
-    inquirer
-      .prompt({
-        message: "Enter your GitHub username:",
-        name: "username"
-      })
-      .then(function({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
-        const queryEmailUrl = `https://api.github.com/users/${username}/events/public`;
-        axios.get(queryUrl).then(function(res) {
-          fs.appendFile(
-            "READMEtest.md",
-            `![profile avatar](${res.data.avatar_url})\n`,
-            function(err) {
-              if (err) {
-                throw err;
-              }
-            }
-          );
-        });
-        axios.get(queryEmailUrl).then(function(pushinfo) {
-          fs.appendFile(
-            "READMEtest.md",
-            //most people do not have their email set to a public profile, this is a sneakier way of getting it
-            `Email me at: ${pushinfo.data[0].payload.commits[0].author.email}\n`,
-            function(err) {
-              if (err) {
-                throw err;
-              }
-            }
-          );
-        });
-      });
-  });
+  },
+  {
+    type: "input",
+    message: "What is your project description?",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What type of license is this?",
+    name: "license"
+  }
+]).then(function(answer) {
+    fs.writeFile("READMETEST.md", 
+    `# Project Title: ${answer.title}\n\n## Description: ${answer.description}\n\n## Table of Contents  
+    1. Installation
+    
+    2. Usage
+    
+    3. License - ${answer.license}
+    
+    4. Contributing
+    
+    5. Tests
+    
+    6. GitHub Info: `, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    })
+})
+
+
+
+// inquirer
+//   .prompt({
+//     type: "input",
+//     message: "What is your project title?",
+//     name: "title"
+//   })
+//   .then(function({ title }) {
+//     fs.appendFile("READMEtest.md", `#Project Title: ${title}\n`, function(err) {
+//       if (err) {
+//         throw err;
+//       }
+//     });
+//   })
+//   .then(function() {
+//     inquirer
+//       .prompt({
+//         message: "Enter your GitHub username:",
+//         name: "username"
+//       })
+//       .then(function({ username }) {
+//         const queryUrl = `https://api.github.com/users/${username}`;
+//         const queryEmailUrl = `https://api.github.com/users/${username}/events/public`;
+//         axios.get(queryUrl).then(function(res) {
+//           fs.appendFile(
+//             "READMEtest.md",
+//             `![profile avatar](${res.data.avatar_url})\n`,
+//             function(err) {
+//               if (err) {
+//                 throw err;
+//               }
+//             }
+//           );
+//         });
+//         axios.get(queryEmailUrl).then(function(pushinfo) {
+//           fs.appendFile(
+//             "READMEtest.md",
+//             //most people do not have their email set to a public profile, this is a sneakier way of getting it
+//             `Email me at: ${pushinfo.data[0].payload.commits[0].author.email}\n`,
+//             function(err) {
+//               if (err) {
+//                 throw err;
+//               }
+//             }
+//           );
+//         });
+//       });
+//   });
 
 // const questions = [];
 
